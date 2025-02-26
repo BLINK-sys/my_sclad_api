7cb7c4a9282d715c1e1382f9d1f474c507edce6b
BLINK-sys <77540383+BLINK-sys@users.noreply.github.com>
2025-02-26 17:27:34 +0500

    Update config_GPT.py

--- config_GPT.py
+++ config_GPT.py
@@ -1,2 +1,2 @@
 token = "sk-proj-T1pSQ24X2z858B5lBZUhg1JprQia84l7GZfLPEpHezEgX9HiTH9mpC185yw9IDZMCZvH1cRhVhT3BlbkFJzvNPIXqHei2Ncf" \
-        "-J6oaRXqnLyFw3dBxWzWdkwOLk4fg8-yESZpfxLhyvCQ7qZRM6yJJpEY8CYA "
+        "-J6oaRXqnLyFw3dBxWzWdkwOLk4fg8-yESZpfxLhyvCQ7qZRM6yJJpEY8CYA"
